3652b2acc548f1bfa0efd0ff0cf86a74b580d9de
Marcelo Zárate <marcelozarate@gmail.com>
2018-03-04 16:12:47 -0300

    Reworked default home

---
+++ client/src/components/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+    <h1>{{ msg }}</h1>
+    <h2>Powered by Vue.js, Node.js, Express and MongoDB</h2>
+    <ul>
+      <li>
+        <router-link v-bind:to="{ name: 'Posts' }">
+          Go to App
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to the MEVN-posts App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
